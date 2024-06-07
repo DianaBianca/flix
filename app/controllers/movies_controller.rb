a4937d17ecfeb1215163c8293903ecc515fd6766
DianaBianca <dianabiancasr@gmail.com>
2024-06-07 17:20:37 -0300

    adding creating new movie view

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,13 +11,29 @@ class MoviesController < ApplicationController
     @movie = Movie.find(params[:id])
   end
 
-  def update
-    @movie = Movie.find(params[:id])
+  def new
+    @movie = Movie.new
+  end
 
-    movie_params = params.require(:movie).permit!
-    
+  def create
+    @movie = Movie.new(movie_params)
+
+    @movie.save
+
+    redirect_to @movie
+  end
+
+  def update
+    @movie = Movie.new(params[:id])
+
     @movie.update(movie_params)
 
     redirect_to @movie
+  end
+
+private
+
+  def movie_params
+    params.require(:movie).permit!
   end
 end
